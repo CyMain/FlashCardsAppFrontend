ed87726ef5bd90a7ea5b0c54c869347ce16c5e4c
CyMain <okikiolacyrus@gmail.com>
2026-01-17 02:00:51 +0100

    feat: add card popup
    -Add Card Pop Up feature
    -Add various List Components
    -Start Quiz View Development
    -Add minimal styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,46 @@
 import { useState } from 'react'
 import './App.css'
 
+
+// PopUp Components
+const CardPopUp = () =>{
+  return(
+    <>
+      <div className="card-pop-up">
+        <div className="container" id='card-pop-up-container'>
+          <div className="card-term-and-definition">
+            <div className="card-term">Term</div>
+            <div className="card-definition">
+              Nothing to see here guys. I have no idea what "term" means.
+            </div>
+          </div>
+          <div className="card-btn-group">
+            <button type="button">Edit</button>
+            <button type="button">Delete</button>
+            <button type="button">Close</button>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+// Screen Components
+const SubScreen = ({message, children}) => {
+  return (
+    <>
+      <div className="screen-fill sub-screen">
+        <h1 className='sub-screen-title'>{message}</h1>
+        <div className="sub-screen-container">
+          {children}
+        </div>
+      </div>
+    </>
+  )
+}
+
 // Auth Components
-const Login = ( 
-  {setRegistering}
-  
-)=>{
+const Login = ({setRegistering})=>{
   console.log("logging in...")
   console.log(setRegistering)
   return(
@@ -26,9 +61,7 @@ const Login = (
   )
 }
 
-const Register = (
-  {setRegistering}
-) => {
+const Register = ({setRegistering}) => {
   console.log("registering...")
   console.log(setRegistering)
   return(
@@ -53,9 +86,7 @@ const Register = (
   )
 }
 
-const AuthPage = (
-  
-) => {
+const AuthPage = () => {
   const [isRegistering, setIsRegistering] = useState(true)
   return(
     <>
@@ -97,18 +128,147 @@ function App() {
   )
 }
 
+const FolderList = ({username}) => {
+  return(
+    <>
+      <SubScreen message={`Greetings, ${username}!`}>
+        <div className="nav-group">
+          <div className="dir-tracking">
+            Folder &gt; Deck &gt; Card
+          </div>
+          <div className="action-btns">
+            <button type="button">Quiz Me!</button>
+            <button type="button">Rename</button>
+            <button type="button">+</button>
+          </div>
+        </div>
+        <ul className="folders-list">
+          <li className="folder">
+            <div className="folder-name">Folder1</div>
+            <div className="folder-actions">
+              <button type="button">Quiz Me!</button>
+              <button type="button">Delete</button>
+              <button type="button">Rename</button>
+            </div>
+          </li>
+          <li className="folder">
+            <div className="folder-name">Folder1</div>
+            <div className="folder-actions">
+              <button type="button">Quiz Me!</button>
+              <button type="button">Delete</button>
+              <button type="button">Rename</button>
+            </div>
+          </li>
+        </ul>
+      </SubScreen>
+    </>
+  )
+}
+
+const DeckList = ({username})=>{
+  return(
+    <>
+      <SubScreen message={`Greetings, ${username}`}>
+        <div className="nav-group">
+          <div className="dir-tracking">
+            Folder &gt; Deck &gt; Card
+          </div>
+          <div className="action-btns">
+            <button type="button">Quiz Me!</button>
+            <button type="button">Rename</button>
+            <button type="button">+</button>
+          </div>
+        </div>
+        <ul className="folders-list">
+          <li className="folder">
+            <div className="folder-name">Tech</div>
+            <div className="folder-actions">
+              <button type="button">Quiz Me!</button>
+              <button type="button">Delete</button>
+              <button type="button">Rename</button>
+            </div>
+          </li>
+          <li className="folder">
+            <div className="folder-name">Cooking</div>
+            <div className="folder-actions">
+              <button type="button">Quiz Me!</button>
+              <button type="button">Delete</button>
+              <button type="button">Rename</button>
+            </div>
+          </li>
+        </ul>
+      </SubScreen>
+    </>
+  )
+}
+
+const CardsList = ({username})=>{
+  return(
+    <>
+      <SubScreen message={`Greetings, ${username}`}>
+        <div className="nav-group">
+          <div className="dir-tracking">
+            Folder &gt; Deck &gt; Card
+          </div>
+          <div className="action-btns">
+            <button type="button">Quiz Me!</button>
+            <button type="button">Rename</button>
+            <button type="button">+</button>
+          </div>
+        </div>
+        <ul className="folders-list">
+          <li className="folder">
+            <div className="folder-name">
+              <div className="item-term">Term</div>
+              <p className="item-definition">This refers to an item with meaning in a vocabulary.</p>
+            </div>
+            <div className="folder-actions">
+              <button type="button">Quiz Me!</button>
+              <button type="button">Delete</button>
+              <button type="button">Edit</button>
+            </div>
+          </li>
+          <li className="folder">
+            <div className="folder-name">
+              <div className="item-term">Term2</div>
+              <p className="item-definition">This refers to the second item with meaning in a vocabulary.</p>
+            </div>
+            <div className="folder-actions">
+              <button type="button">Quiz Me!</button>
+              <button type="button">Delete</button>
+              <button type="button">Edit</button>
+            </div>
+          </li>
+          <li className="folder">
+            <div className="folder-name">
+              <div className="item-term">Term3</div>
+              <p className="item-definition">This refers to the third item with meaning in a vocabulary.</p>
+            </div>
+            <div className="folder-actions">
+              <button type="button">Quiz Me!</button>
+              <button type="button">Delete</button>
+              <button type="button">Edit</button>
+            </div>
+          </li>
+        </ul>
+      </SubScreen>
+    </>
+  )
+}
+
 const MainApp = (
   {username}
 ) =>{
-  const [view, setView] = useState("folder");
+  const [view, setView] = useState("quiz");
+  const cardPopUp = false
   const currFolder = {};
   const currDeck = {};
   const currCard = {};
 
   return(
     <>
-      {
-        view === "main" ?
+      {cardPopUp && <CardPopUp/>}
+      {view === "main" &&
         (
           <div className="screen-fill main-screen">
             <h1 className='screen-title'>Greetings, {username}!</h1>
@@ -119,49 +279,39 @@ const MainApp = (
               </div>
             </div>
           </div>
-        ) : (
-          view === "folder" ?
-          (
-            <div className="screen-fill sub-screen">
-              <h1 className='sub-screen-title'>Greetings, {username}!</h1>
-              <div className="sub-screen-container">
-                <div className="nav-group">
-                  <div className="dir-tracking">
-                    Folder &gt; Deck &gt; Card &gt;
-                  </div>
-                  <div className="action-btns">
-                    <button type="button">Quiz Me!</button>
-                    <button type="button">Rename</button>
-                    <button type="button">+</button>
-                  </div>
-                </div>
-                <ul className="folders-list">
-                  <li className="folder">
-                    <div className="folder-name">Folder1</div>
-                    <div className="folder-actions">
-                      <button type="button">Quiz Me!</button>
-                      <button type="button">Delete</button>
-                      <button type="button">Rename</button>
-                    </div>
-                  </li>
-                  <li className="folder">
-                    <div className="folder-name">Folder1</div>
-                    <div className="folder-actions">
-                      <button type="button">Quiz Me!</button>
-                      <button type="button">Delete</button>
-                      <button type="button">Rename</button>
-                    </div>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          )
-          :
-          (
-            ""
-          )
+        )}
+
+      {view === "folder"&&(
+            <FolderList username={username}/>
+          )}
+      {
+        view === "deck" && (
+          <DeckList username={username}/>
+        )}
+      {
+        view === "cards" && (
+          <CardsList username={username}/>
+        )}
+      {
+        view === "quiz" && (
+          <QuizView/>
         )
       }
+    </>
+  )
+}
+
+const QuizView = () => {
+  let num_rounds = 0
+  return(
+    <>
+      <SubScreen message={`Quiz Time!`}>
+        <div className="quiz-container">
+          <label htmlFor="">How many questions would you like to be asked? <input type="number" placeholder='pick a number' className='num-rounds-input' min={1}/></label>
+          <button type="button">Start Quiz!</button>
+        </div>
+        
+      </SubScreen>
     </>
   )
 }
